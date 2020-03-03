f10d1c9a80b8a93afce7769d238979a300a93366
Nike <nike.bliznyuk@gmail.com>
2020-03-03 23:41:36 +0600

    implemented clear(), save(), get(), getAll(), size()

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,49 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < this.size(); i++)
+            storage[i] = null;
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = resume;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < this.size(); i++) {
+            if (storage[i].uuid == uuid)
+                return storage[i];
+        }
         return null;
     }
 
     void delete(String uuid) {
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] tempArray = new Resume[this.size()];
+        for (int i = 0; i < this.size(); i++)
+            tempArray[i] = storage[i];
+
+        return tempArray;
     }
 
     int size() {
-        return 0;
+        int size = 0;
+        for (int i = 0; i < storage.length; i++) {
+            size = i;
+            if (storage[i] == null)
+                return size;
+        }
+        return size;
     }
 }
