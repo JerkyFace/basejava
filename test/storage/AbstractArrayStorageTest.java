5cf93b675b398801a160214aba4252a54dc0dd37
JerkFace <nike.bliznyuk@gmail.com>
2020-12-14 16:40:24 +0600

    hw4 done

--- test/storage/AbstractArrayStorageTest.java
+++ test/storage/AbstractArrayStorageTest.java
@@ -1,5 +1,8 @@
 package storage;
 
+import exception.ExistStorageException;
+import exception.NotExistStorageException;
+import exception.StorageException;
 import model.Resume;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,41 +10,93 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AbstractArrayStorageTest {
-    private Storage storage;
+    private static final int INITIAL_AMOUNT_OF_RESUMES = 10;
+
+    private final Storage storage;
+
+    protected AbstractArrayStorageTest(Storage storage) {
+        this.storage = storage;
+    }
 
     @BeforeEach
-    public void setUp() throws Exception {
+    public void setUp() {
         storage.clear();
-        storage.save(new Resume());
-        storage.save(null);
-        storage.save(new Resume());
+        for (int i = 1; i <= INITIAL_AMOUNT_OF_RESUMES; i++) {
+            storage.save(new Resume("uuid".concat(String.valueOf(i))));
+        }
     }
 
     @Test
     void clear() {
+        storage.clear();
+        assertEquals(0, storage.size());
     }
 
     @Test
     void update() {
+        Resume updatedResume = new Resume("uuid5");
+        storage.update(updatedResume);
+        assertSame(storage.get("uuid5"), updatedResume);
+        assertThrows(NotExistStorageException.class, () -> storage.update(new Resume("non existent resume")));
     }
 
     @Test
     void save() {
+        Resume resume = new Resume("uuid11");
+        storage.save(resume);
+        assertEquals(INITIAL_AMOUNT_OF_RESUMES + 1, storage.size());
+        assertSame(resume, storage.get("uuid11"));
+        assertThrows(ExistStorageException.class, () -> storage.save(resume));
+    }
+
+    @Test
+    void saveIntoFullStorage() {
+        int storageLimit = 100;
+        try {
+            for (int i = storage.size() + 1; i <= storageLimit; i++) {
+                storage.save(new Resume("uuid".concat(String.valueOf(i))));
+            }
+        } catch (StorageException e) {
+            fail("Exception should not be thrown.");
+        }
+
+        Exception exception = assertThrows(StorageException.class, () -> storage.save(new Resume("odd resume")));
+
+        String expectedExceptionMessage = "Unable to add resume with uuid 'odd resume'. The storage is full.";
+        assertTrue(exception.getMessage().contains(expectedExceptionMessage));
     }
 
     @Test
     void size() {
+        assertEquals(INITIAL_AMOUNT_OF_RESUMES, storage.size());
     }
 
     @Test
     void get() {
+        for (int i = 1; i <= storage.size(); i++) {
+            assertEquals(storage.get("uuid".concat(String.valueOf(i))),
+                    new Resume("uuid".concat(String.valueOf(i))));
+        }
+        assertThrows(NotExistStorageException.class, () -> storage.get("non existent resume"));
     }
 
     @Test
     void delete() {
+        String uuid = "uuid1";
+        storage.delete(uuid);
+        assertEquals(INITIAL_AMOUNT_OF_RESUMES - 1, storage.size());
+        assertThrows(NotExistStorageException.class, () -> {
+            storage.get(uuid);
+            storage.get("non existent resume");
+        });
     }
 
     @Test
     void getAll() {
+        Resume[] resumes = storage.getAll();
+        assertEquals(10, resumes.length);
+        for (Resume resume : resumes) {
+            assertSame(resume, storage.get(resume.getUuid()));
+        }
     }
 }
