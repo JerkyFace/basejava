58ef6036e04fc80a691f494273040ebe0a42f6ae
Nike <nike.bliznyuk@gmail.com>
2021-01-05 20:28:14 +0600

    hw6 first commit

--- src/test/MainTestArrayStorage.java
+++ src/test/MainTestArrayStorage.java
@@ -1,18 +1,22 @@
 package test;
 
 import model.Resume;
-import storage.arraybased.SortedArrayStorage;
+import storage.Storage;
+import storage.listbased.ListStorage;
 
 
 /**
  * Test for your ArrayStorage implementation
  */
 public class MainTestArrayStorage {
-    static final SortedArrayStorage ARRAY_STORAGE = new SortedArrayStorage();
-
+    static final Storage ARRAY_STORAGE = new ListStorage();
 
     public static void main(String[] args) {
+        ARRAY_STORAGE.save(new Resume("1", "1"));
+        ARRAY_STORAGE.save(new Resume("2", "2"));
+        ARRAY_STORAGE.save(new Resume("3", "3"));
 
+        System.out.println(ARRAY_STORAGE.getAllSorted().indexOf(new Resume("2", "")));
     }
 
     static void printAll() {
