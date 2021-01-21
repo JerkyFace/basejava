49e96e689b711629dfdaf25abc326769a58f3b86
Никита Близнюк <nbliznyuk@digitalleague.ru>
2021-01-21 16:39:20 +0600

    hw9 recursive directory walk done

--- src/MainFile.java
+++ src/MainFile.java
@@ -5,25 +5,22 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.Objects;
 
 public class MainFile {
     // print all files recursively task
-    public static void printCatalogContent(File catalog) {
-        String dash = "--";
-        String spaces = "  ";
-        String dirs = "";
-        String filez = "";
-        boolean flag = false;
-
+    public static void printCatalogContent(File catalog, int level) {
         File[] files = catalog.listFiles();
-        for (File file : Objects.requireNonNull(files)) {
-            if (file.isDirectory()) {
-                flag = true;
-                System.out.println(file);
-                printCatalogContent(file);
-            } else {
-                System.out.println(file.getName());
+        if (files != null) {
+            for (File file : files) {
+                for (int i = 0; i < level; i++) {
+                    System.out.print("__");
+                }
+                if (file.isDirectory()) {
+                    System.out.println(file.getName() + "▼");
+                    printCatalogContent(file, level + 1);
+                } else {
+                    System.out.println(file.getName());
+                }
             }
         }
     }
@@ -32,7 +29,7 @@ public class MainFile {
         File file = new File("./", "test.txt");
         File directory = new File("./");
 
-        printCatalogContent(directory);
+        printCatalogContent(directory, 0);
 
         System.out.println(file.getCanonicalPath());
         if (file.createNewFile()) {
@@ -42,8 +39,6 @@ public class MainFile {
                     .toString()
                     .getBytes());
         }
-
-
         try {
             System.out.println(Files.readAllLines(file.toPath()));
         } catch (IOException e) {
