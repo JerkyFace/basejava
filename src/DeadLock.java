f7bd2bfce82029cce76fd815fccf56e6fa0348eb
Nick <nike.bliznyuk@gmail.com>
2021-02-10 09:45:07 +0300

    hw11 deadlock simplified

--- src/DeadLock.java
+++ src/DeadLock.java
@@ -1,35 +1,22 @@
-class SharedResource {
-    String resourceName;
-
-    public SharedResource(String resourceName) {
-        this.resourceName = resourceName;
-    }
-
-    public String getResourceName() {
-        return resourceName;
-    }
-}
-
 public class DeadLock implements Runnable {
 
-
-    private final SharedResource resource1 = new SharedResource("ресурс1");
-    private final SharedResource resource2 = new SharedResource("ресурс2");
+    private final String resource1 = "ресурс 1";
+    private final String resource2 = "ресурс 2";
 
     private void job1() throws InterruptedException {
-        UseResource(resource2, resource1);
-    }
-
-    private void job2() throws InterruptedException {
         UseResource(resource1, resource2);
     }
 
-    private void UseResource(final SharedResource resource1, final SharedResource resource2) throws InterruptedException {
+    private void job2() throws InterruptedException {
+        UseResource(resource2, resource1);
+    }
+
+    private void UseResource(final String resource1, final String resource2) throws InterruptedException {
         synchronized (resource1) {
-            System.out.println(Thread.currentThread().getName() + " удерживает " + resource1.getResourceName());
+            System.out.println(Thread.currentThread().getName() + " удерживает " + resource1);
             Thread.sleep(5);
             synchronized (resource2) {
-                System.out.println(Thread.currentThread().getName() + " удерживает " + resource2.getResourceName());
+                System.out.println(Thread.currentThread().getName() + " удерживает " + resource1 + " и " + resource2);
             }
         }
     }
@@ -40,7 +27,6 @@ public class DeadLock implements Runnable {
             job1();
             job2();
         } catch (Exception ignore) {
-
         }
     }
 
