e6abf26babee8779322c9e01db70cf8ab271a1bd
Nike <nike.bliznyuk@gmail.com>
2021-02-10 21:54:03 +0600

    hw12 first commit

--- src/MainConcurrency.java
+++ src/MainConcurrency.java
@@ -1,10 +1,13 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MainConcurrency {
     public static final int THREADS_NUMBER = 10000;
-    private int counter;
-    private static final Object LOCK = new Object();
+    private final AtomicInteger atomicCounter = new AtomicInteger();
+
+    private static final ThreadLocal<SimpleDateFormat> threadLocal = ThreadLocal.withInitial(SimpleDateFormat::new);
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println(Thread.currentThread().getName());
@@ -13,52 +16,40 @@ public class MainConcurrency {
             @Override
             public void run() {
                 System.out.println(getName() + ", " + getState());
-                throw new IllegalStateException();
             }
         };
         thread0.start();
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState());
-            }
-
-            private void inc() {
-                synchronized (this) {
-                    //counter++
-                }
-            }
-        }).start();
+        new Thread(() -> System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState())).start();
 
         System.out.println(thread0.getState());
 
         final MainConcurrency mainConcurrency = new MainConcurrency();
-        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
+        CountDownLatch latch = new CountDownLatch(THREADS_NUMBER);
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
         for (int i = 0; i < THREADS_NUMBER; i++) {
-            Thread thread = new Thread(() -> {
+
+            Future<Integer> future = executorService.submit(() ->
+            {
                 for (int j = 0; j < 100; j++) {
                     mainConcurrency.increment();
+                    System.out.println(threadLocal.get().format(new Date()));
                 }
+                latch.countDown();
+                return 5;
             });
-            thread.start();
-            threads.add(thread);
         }
 
-        threads.forEach(t -> {
-            try {
-                t.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-        System.out.println(mainConcurrency.counter);
+        latch.await(10, TimeUnit.SECONDS);
+        executorService.shutdown();
+        System.out.println(mainConcurrency.atomicCounter.get());
+
     }
 
     private synchronized void increment() {
         // synchronized (this) - эквивалентно модификатору метода synchronized, генерит меньше байт кода
         // synchronized (MainConcurrency.class) - если метод статический, компилятор локает объект .class
-        counter++;
+        atomicCounter.incrementAndGet();
     }
 }
