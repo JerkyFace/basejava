93e3c621512077c0e708d234f9ad724477038fc8
Nike <nike.bliznyuk@gmail.com>
2021-02-17 01:00:05 +0600

    hw12

--- src/MainStreams.java
+++ src/MainStreams.java
@@ -3,9 +3,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class MainStreams {
+    private static final int DECIMAL_DIVIDER = 10;
+
     public static void main(String[] args) {
-        List<Integer> integers = Arrays.asList(1, 1, 1, 1, 1, 1, 1);
-        oddOrEven(integers).forEach(System.out::println);
+        List<Integer> integers = Arrays.asList(1, 2, 1, 4, 1, 2, 1); //1111
+        oddOrEven(integers).forEach(System.out::print);
+
+        System.out.println();
+
+        int[] array = {7, 7, 2, 4, 4, 3}; //2347
+        System.out.println(minValue(array));
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
@@ -15,6 +22,13 @@ public class MainStreams {
     }
 
     private static int minValue(int[] values) {
-        return 0; //todo
+        int minValue = 0;
+        int[] arrayWithoutDublicates = Arrays.stream(values).distinct().sorted().toArray();
+        int power = (int) Math.pow(DECIMAL_DIVIDER, arrayWithoutDublicates.length - 1);
+        for (int value : arrayWithoutDublicates) {
+            minValue += value * power;
+            power /= DECIMAL_DIVIDER;
+        }
+        return minValue;
     }
 }
