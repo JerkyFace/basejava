f826aabddda93df149069f0c752f6be630d81474
Nike <nike.bliznyuk@gmail.com>
2021-03-14 14:17:37 +0600

    hw13: refactored

--- src/resumeapp/storage/dbbased/DataBaseStorage.java
+++ src/resumeapp/storage/dbbased/DataBaseStorage.java
@@ -1,118 +1,107 @@
 package resumeapp.storage.dbbased;
 
-import org.postgresql.util.PSQLException;
-import resumeapp.exception.ExistStorageException;
 import resumeapp.exception.NotExistStorageException;
-import resumeapp.exception.StorageException;
 import resumeapp.model.Resume;
-import resumeapp.sql.ConnectionFactory;
 import resumeapp.storage.Storage;
+import resumeapp.util.sql.SqlHelper;
 
-import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DataBaseStorage implements Storage {
-    public final ConnectionFactory connectionFactory;
+    public final SqlHelper sqlHelper;
 
     public DataBaseStorage(String dbUrl, String dbUser, String dbPassword) {
-        connectionFactory = () -> DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+        sqlHelper = new SqlHelper(() -> DriverManager.getConnection(dbUrl, dbUser, dbPassword));
     }
 
     @Override
     public void clear() {
-        try (Connection connection = connectionFactory.getConnection();
-             PreparedStatement statement = connection.prepareStatement("DELETE FROM resume")) {
-            statement.execute();
-        } catch (SQLException sqle) {
-            throw new StorageException("Could not clear table", sqle);
-        }
+        sqlHelper.execute("DELETE FROM resume",
+                "Could not clear table",
+                PreparedStatement::execute);
     }
 
     @Override
     public void update(Resume resume) {
-        try (Connection connection = connectionFactory.getConnection();
-             PreparedStatement statement =
-                     connection.prepareStatement("UPDATE resume SET full_name = ? WHERE uuid = ?")) {
-            statement.setString(1, resume.getFullName());
-            statement.setString(2, resume.getUuid());
-            if (statement.executeUpdate() == 0) {
-                throw new NotExistStorageException(resume.getUuid());
-            }
-        } catch (SQLException sqle) {
-            throw new StorageException("Could not update record", sqle);
-        }
+        sqlHelper.execute("UPDATE resume SET full_name = ? WHERE uuid = ?",
+                "Could not update record",
+                statement -> {
+                    statement.setString(1, resume.getFullName());
+                    statement.setString(2, resume.getUuid());
+                    if (statement.executeUpdate() == 0) {
+                        throw new NotExistStorageException(resume.getUuid());
+                    }
+                    return null;
+                });
     }
 
     @Override
     public void save(Resume resume) {
-        try (Connection connection = connectionFactory.getConnection();
-             PreparedStatement statement =
-                     connection.prepareStatement("INSERT INTO resume (uuid, full_name) VALUES (?, ?)")) {
-            statement.setString(1, resume.getUuid());
-            statement.setString(2, resume.getFullName());
-            statement.execute();
-        } catch (Exception e) {
-            if (e instanceof PSQLException && ((PSQLException) e).getSQLState().equals("23505")) {
-                throw new ExistStorageException(resume.getUuid());
-            }
-            throw new StorageException("Could not save any data", e);
-        }
+        sqlHelper.execute("INSERT INTO resume (uuid, full_name) VALUES (?, ?)",
+                "Could not save any data",
+                statement -> {
+                    statement.setString(1, resume.getUuid());
+                    statement.setString(2, resume.getFullName());
+                    if (statement.executeUpdate() == 0) {
+                        throw new NotExistStorageException(resume.getUuid());
+                    }
+                    return null;
+                });
     }
 
     @Override
     public Resume get(String uuid) {
-        try (Connection connection = connectionFactory.getConnection();
-             PreparedStatement statement = connection.prepareStatement("SELECT * FROM resume WHERE uuid = ?")) {
-            statement.setString(1, uuid);
-            ResultSet rs = statement.executeQuery();
-            if (!rs.next()) {
-                throw new NotExistStorageException(uuid);
-            }
-            return new Resume(uuid, rs.getString("full_name"));
-        } catch (SQLException sqle) {
-            throw new StorageException("Could not get any data", sqle);
-        }
+        return sqlHelper.execute("SELECT * FROM resume WHERE uuid = ?",
+                "Could not get any data",
+                statement -> {
+                    statement.setString(1, uuid);
+                    ResultSet rs = statement.executeQuery();
+                    if (!rs.next()) {
+                        throw new NotExistStorageException(uuid);
+                    }
+                    return new Resume(uuid, rs.getString("full_name"));
+                });
     }
 
     @Override
     public void delete(String uuid) {
-        try (Connection connection = connectionFactory.getConnection();
-             PreparedStatement statement = connection.prepareStatement("DELETE FROM resume WHERE uuid = ?")) {
-            statement.setString(1, uuid);
-            statement.execute();
-        } catch (SQLException sqle) {
-            throw new StorageException("Could not delete any data", sqle);
-        }
+        sqlHelper.execute("DELETE FROM resume WHERE uuid = ?",
+                "Could not delete data",
+                statement -> {
+                    statement.setString(1, uuid);
+                    statement.execute();
+                    return null;
+                });
     }
 
     @Override
     public List<Resume> getAllSorted() {
         List<Resume> list = new ArrayList<>();
-        try (Connection connection = connectionFactory.getConnection();
-             PreparedStatement statement =
-                     connection.prepareStatement("SELECT uuid, full_name FROM resume ORDER BY full_name")) {
-            ResultSet rs = statement.executeQuery();
-            while (rs.next()) {
-                list.add(new Resume(rs.getString("uuid").trim(), rs.getString("full_name")));
-            }
-        } catch (SQLException sqle) {
-            sqle.printStackTrace();
-        }
-        return list;
+        return sqlHelper.execute("SELECT uuid, full_name FROM resume ORDER BY full_name",
+                "Could not get any data",
+                statement -> {
+                    ResultSet rs = statement.executeQuery();
+                    while (rs.next()) {
+                        list.add(new Resume(rs.getString("uuid").trim(), rs.getString("full_name")));
+                    }
+                    return list;
+                });
     }
 
     @Override
     public int size() {
-        try (Connection connection = connectionFactory.getConnection();
-             PreparedStatement statement = connection.prepareStatement("SELECT count(*) as count FROM resume ")) {
-            ResultSet rs = statement.executeQuery();
-            if (!rs.next()) {
-                throw new NotExistStorageException("Storage is empty");
-            }
-            return rs.getInt("count");
-        } catch (SQLException sqle) {
-            throw new StorageException("Could not get any data", sqle);
-        }
+        return sqlHelper.execute("SELECT count(*) as count FROM resume",
+                "Could not get any data",
+                statement -> {
+                    ResultSet rs = statement.executeQuery();
+                    if (!rs.next()) {
+                        throw new NotExistStorageException("Storage is empty");
+                    }
+                    return rs.getInt("count");
+                });
     }
 }
